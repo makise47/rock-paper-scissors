fdfe66ef2e1b9baf816df2c8dbed46c1372d4eb6
Makise <servicemailfor@gmail.com>
2021-07-19 22:31:19 +0530

    First styled page design.

--- index.md
+++ index.md
@@ -4,23 +4,29 @@
         <meta charset = "utf-8">
         <title>Rock Paper Scissors</title>
         <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto">
         <script type="text/javascript" src="script/index.js"></script>
     </head>
 
     <body>
-        <div class="main">
-            <h1 class="title">Play a game of Rock-Paper-Scissors with Makise!</h1>
-            <h2 class="info">First player to get to 5 wins!</h2>
-        </div>
+        <div class="container">
+            <div class="main">
+                <h1 class="title">Play a game of Rock-Paper-Scissors with Makise!</h1>
+                <h2 class="info">First player to get to 5 wins!</h2>
+            </div>
 
-        <div class="buttons"> 
-            <input type="button" class="rock" value="Rock" onclick="rockfn()">
-            <input type="button" class="paper" value="Paper" onclick="paperfn()">
-            <input type="button" class="scissors" value="Scissors" onclick="scissorsfn()">
-        </div>
-        
-        <div class="opbox">
-            <p id="opline"></p>
+            <div class="buttons"> 
+                <input type="button" class="rock" value="Rock" onclick="rockfn()">
+                <input type="button" class="paper" value="Paper" onclick="paperfn()">
+                <input type="button" class="scissors" value="Scissors" onclick="scissorsfn()">
+            </div>
+            
+            <div class="opbox">
+                <div id=></div>
+                <p id="opline"></p>
+                <p id="scorecard"></p>
+            </div>
         </div>
     </body>
 </html>
--- script/index.js
+++ script/index.js
@@ -8,22 +8,28 @@ function rockfn() {
         let botchoice = Math.random();
         if (botchoice > 0.66) {
             ++userwins;
-            document.getElementById("opline").innerHTML+=`Rock beats scissor! You have ${userwins} points while Makise has ${botwins} points! <br/>`;
+            document.getElementById("opline").innerHTML+=`You picked Rock while Makise picked Scissors! You get a point! <br/>`;
+            document.getElementById("opline").innerHTML+=`<br/>`
+            document.getElementById("scorecard").innerHTML=`Your points: ${userwins}. Makise's points: ${botwins}. <br/>`
         }
         else if (botchoice < 0.33) {
-            document.getElementById("opline").innerHTML+=`It's a tie! You have ${userwins} points while Makise has ${botwins} points! <br/>`;
+            document.getElementById("opline").innerHTML+=`Both of you picked Rock! It's a tie. <br/>`;
+            document.getElementById("opline").innerHTML+=`<br/>`
+            document.getElementById("scorecard").innerHTML=`Your points: ${userwins}. Makise's points: ${botwins}. <br/>`
         }
         else {
             ++botwins;
-            document.getElementById("opline").innerHTML+=`Aww rock loses to paper! You have ${userwins} points while Makise has ${botwins} points! <br/>`;
+            document.getElementById("opline").innerHTML+=`You picked Rock while Makise picked Paper. You lose this round. <br/>`;
+            document.getElementById("opline").innerHTML+=`<br/>`
+            document.getElementById("scorecard").innerHTML=`Your points: ${userwins}. Makise's points: ${botwins}. <br/>`
         }
     }
     if (userwins >=5 && end != 1) {
-        document.getElementById("opline").innerHTML+=`<br/> Yay! You have won the game! <br/> Reload the page to play again!`;
+        document.getElementById("scorecard").innerHTML+=`<br/> Yay! You have won the game! <br/> Reload the page to play again!`;
         end = 1;
     }
     if (botwins >=5 && end != 1) {
-        document.getElementById("opline").innerHTML+=`<br/> Tough luck :( You lost to Makise. <br/> Reload the page to play again!`;
+        document.getElementById("scorecard").innerHTML+=`<br/> Tough luck :( You lost to Makise. <br/> Reload the page to play again!`;
         end = 1;
     }
 } 
@@ -34,22 +40,28 @@ function paperfn() {
         let botchoice = Math.random();
         if (botchoice > 0.66) {
             ++botwins;
-            document.getElementById("opline").innerHTML+=`Aww paper loses to scissors! You have ${userwins} points while Makise has ${botwins} points! <br/>`;
+            document.getElementById("opline").innerHTML+=`You picked Paper while Makise picked Scissors. You lose this round. <br/>`;
+            document.getElementById("opline").innerHTML+=`<br/>`
+            document.getElementById("scorecard").innerHTML=`Your points: ${userwins}. Makise's points: ${botwins}. <br/>`
         }
         else if (botchoice < 0.33) {
             ++userwins;
-            document.getElementById("opline").innerHTML+=`Paper beats rock! You have ${userwins} points while Makise has ${botwins} points! <br/>`;
+            document.getElementById("opline").innerHTML+=`You picked Paper while Makise picked Rock! You get a point! <br/>`;
+            document.getElementById("opline").innerHTML+=`<br/>`
+            document.getElementById("scorecard").innerHTML=`Your points: ${userwins}. Makise's points: ${botwins}. <br/>`
         }
         else {
-            document.getElementById("opline").innerHTML+=`It's a tie! You have ${userwins} points while Makise has ${botwins} points! <br/>`;
+            document.getElementById("opline").innerHTML+=`Both of you picked Paper! It's a tie. <br/>`;
+            document.getElementById("opline").innerHTML+=`<br/>`
+            document.getElementById("scorecard").innerHTML=`Your points: ${userwins}. Makise's points: ${botwins}. <br/>`
         }
     }
     if (userwins >=5 && end != 1) {
-        document.getElementById("opline").innerHTML+=`<br/> Yay! You have won the game! <br/> Reload the page to play again!`;
+        document.getElementById("scorecard").innerHTML+=`<br/> Yay! You have won the game! <br/> Reload the page to play again!`;
         end = 1;
     }
     if (botwins >=5 && end != 1) {
-        document.getElementById("opline").innerHTML+=`<br/> Tough luck :( You lost to Makise. <br/> Reload the page to play again!`;
+        document.getElementById("scorecard").innerHTML+=`<br/> Tough luck :( You lost to Makise. <br/> Reload the page to play again!`;
         end = 1;
     }
 }
@@ -60,23 +72,29 @@ function scissorsfn() {
     if (end != 1) {
         let botchoice = Math.random();
         if (botchoice > 0.66) {
-            document.getElementById("opline").innerHTML+=`It's a tie! You have ${userwins} points while Makise has ${botwins} points! <br/>`;
+            document.getElementById("opline").innerHTML+=`Both of you picked Scissors! It's a tie. <br/>`;
+            document.getElementById("opline").innerHTML+=`<br/>`
+            document.getElementById("scorecard").innerHTML=`Your points: ${userwins}. Makise's points: ${botwins}. <br/>`
         }
         else if (botchoice < 0.33) {
             ++botwins;
-            document.getElementById("opline").innerHTML+=`Aww scissor loses to rock! You have ${userwins} points while Makise has ${botwins} points! <br/>`;
+            document.getElementById("opline").innerHTML+=`You picked Scissors while Makise picked Rock. You lose this round. <br/>`;
+            document.getElementById("opline").innerHTML+=`<br/>`
+            document.getElementById("scorecard").innerHTML=`Your points: ${userwins}. Makise's points: ${botwins}. <br/>`
         }
         else {
             ++userwins;
-            document.getElementById("opline").innerHTML+=`Scissor beats paper! You have ${userwins} points while Makise has ${botwins} points! <br/>`;
+            document.getElementById("opline").innerHTML+=`You picked Scissors while Makise picked Paper! You get a point! <br/>`;
+            document.getElementById("opline").innerHTML+=`<br/>`
+            document.getElementById("scorecard").innerHTML=`Your points: ${userwins}. Makise's points: ${botwins}. <br/>`
         }
     }
     if (userwins >=5 && end != 1) {
-        document.getElementById("opline").innerHTML+=`<br/> Yay! You have won the game! <br/> Reload the page to play again!`;
+        document.getElementById("scorecard").innerHTML+=`<br/> Yay! You have won the game! <br/> Reload the page to play again!`;
         end = 1;
     }
     if (botwins >=5 && end != 1) {
-        document.getElementById("opline").innerHTML+=`<br/> Tough luck :( You lost to Makise. <br/> Reload the page to play again!`;
+        document.getElementById("scorecard").innerHTML+=`<br/> Tough luck :( You lost to Makise. <br/> Reload the page to play again!`;
         end = 1;
     }
 }
